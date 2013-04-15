128e014de049ab800bfdf899b2338dd7a595c3df
ashaninBenjamin <ashanin.benjamin@gmail.com>
2013-04-15 13:55:59 +0400

    corrected user_sign_in_type, locales, routes

--- app/types/user_sign_in_type.rb
+++ app/types/user_sign_in_type.rb
@@ -1,8 +1,17 @@
 class UserSignInType < User
+  include ActiveModel::Validations
   include BaseType
 
   validates :login, presence: true
   validates :password, presence: true
+
+  validates_each :login do |record, attr, value|
+    user = record.user
+
+    if !user.try(:authenticate, record.password)
+      record.errors.add(attr, :user_or_password_invalid)
+    end
+  end
 
   def user
     @user ||= User.find_by_login(login)
