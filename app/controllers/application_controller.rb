5b09e39fcb607ed7234398a892576e3b7a26ed2b
ashaninBenjamin <ashanin.benjamin@gmail.com>
2013-04-18 19:25:37 +0400

    changed work with auth_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include AuthHelper
 
+  helper_method :signed_in?, :current_user
+
   private
 
   def update_session_history
