ec13a7d044bf14620d73a3d129d494e60a6a19af
ashaninBenjamin <ashanin.benjamin@gmail.com>
2013-04-25 17:24:53 +0400

    removed excess filters

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,6 +1,5 @@
 #coding: utf-8
 class HelpsController < ApplicationController
-  before_filter :super_admin
 
   def index
     @helps = HelpDecorator.decorate_collection Help.by_number
@@ -9,12 +8,6 @@ class HelpsController < ApplicationController
   def show
     @help = Help.find(params[:id]).decorate
     @helps = HelpDecorator.decorate_collection Help.by_number
-  end
-
-  private
-
-  def super_admin
-    redirect_to admin_news_index_path if current_user.super_admin?
   end
 
 end
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,22 +1,13 @@
 #coding: utf-8
 class NewsController < ApplicationController
-  before_filter :super_admin
   # GET /news
-  # GET /news.json
   def index
     @news = News.by_created_at
   end
 
   # GET /news/1
-  # GET /news/1.json
   def show
     @news = News.find(params[:id])
-  end
-
-  private
-
-  def super_admin
-    redirect_to admin_news_index_path if current_user.super_admin?
   end
 
 end
