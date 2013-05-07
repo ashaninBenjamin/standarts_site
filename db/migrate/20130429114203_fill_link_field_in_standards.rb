102a36b1e532e2039b3020d760724f68b0fa566d
ashaninBenjamin <ashanin.benjamin@gmail.com>
2013-05-07 17:08:58 +0400

    changed migration filling link in standards, cause decorate.link haven't exist already

--- db/migrate/20130429114203_fill_link_field_in_standards.rb
+++ db/migrate/20130429114203_fill_link_field_in_standards.rb
@@ -2,7 +2,13 @@ class FillLinkFieldInStandards < ActiveRecord::Migration
   def up
     Standard.transaction do
       Standard.find_each do |standard|
-        standard.link = standard.decorate.link
+        link = standard.number.to_s
+        temp = standard
+        while temp.parent && temp.parent.number.nonzero?
+          temp = temp.parent
+          link = "#{temp.number}-#{link}"
+        end
+        standard.link = link
         standard.save!
       end
     end
